97db79b42b16f0c9495d2c537610bab4d53aec82
zclv <996454914@qq.com>
2018-07-13 11:04:06 +0800

    抽象执行方法

---
+++ delegateSql/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateSql
+{
+    public class Service
+    {
+        public T Execute<T>(Func<SqlCommand, T> func, string connectionString)
+        {
+            SqlTransaction tran = null;
+            try
+            {
+                T t = default(T);
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    tran = conn.BeginTransaction();
+                    {
+                        SqlCommand cmd = new SqlCommand();
+                          t = func.Invoke(cmd);
+                    }
+                    tran.Commit();
+                    return t;
+                }
+            }
+            catch (Exception)
+            {
+                tran.Rollback();
+                throw;
+            }
+        }
+    }
+}
